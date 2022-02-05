e5d0bb73edeef7f99bb33b620ea14091b272e9a5
skewballfox <joshua.ferguson.273@gmail.com>
2022-02-05 16:13:03 -0600

    finished symetric pad, starting on edge

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,7 @@
-use ndarray::{Array, ArrayBase, Axis, Dimension, Slice};
+use ndarray::{
+    Array, Array2, ArrayBase, ArrayView, ArrayView2, ArrayViewMut, ArrayViewMut2, Axis, Dim,
+    Dimension, Slice,
+};
 
 //for all the stuff that doesn't exist
 
@@ -16,6 +19,14 @@ enum PadType {
     Constant,
     Symmetric,
     Edge, //may add more
+}
+enum EdgeRowOp {
+    Top,
+    Bottom,
+}
+enum EdgeColOp {
+    Left,
+    Right,
 }
 pub fn tile<A, S, D>(arr: &ArrayBase<S, D>, reps: &ArrayBase<S, D>) -> Array<A, D>
 where
@@ -35,7 +46,7 @@ where
 /// TODO: need to replace last arg in call: "edge"
 /// see: https://numpy.org/doc/stable/reference/generated/numpy.pad.html?highlight=pad#numpy.pad
 pub fn pad<S, A, D>(
-    arr: &ArrayBase<S, D>,
+    arr: &Array<A, D>,
     pad_width: Vec<[usize; 2]>,
     const_value: A,
     pad_type: PadType, //Enum?
@@ -63,12 +74,6 @@ where
         // Select portion of padded array that needs to be copied from the
         // original array.
         let mut orig_portion = padded.view_mut();
-        for (axis, &[pre_ax, post_ax]) in pad_width.iter().enumerate() {
-            orig_portion.slice_axis_inplace(
-                Axis(axis),
-                Slice::from(pre_ax as isize..padded_shape[axis] as isize - (post_ax as isize)),
-            );
-        }
         // Copy the data from the original array.
         orig_portion.assign(arr);
         match pad_type {
@@ -80,7 +85,7 @@ where
                 array([2, 1, 1, 2, 3, 4, 5, 5, 4, 3])
                 */
 
-                todo!()
+                symmetric_pad(&mut padded, arr, pad_width);
             }
             PadType::Edge => {
                 /*
@@ -95,36 +100,38 @@ where
     padded
 }
 
-fn symmetric_pad<S, A, D>(
-    a: &ArrayBase<S, D>,
+fn symmetric_pad<A, D>(
+    padded_array: &mut Array<A, D>,
+    input_array: &Array<A, D>,
     pad_width: Vec<[usize; 2]>,
-    const_value: A,
-    pad_type: PadType, //Enum?
-) -> Array<A, D>
-where
-    S: ndarray::Data<Elem = A>,
+) where
     A: Clone,
     D: Dimension,
 {
-    let mut a_inv = a.clone();
+    let mut a_inv = input_array.clone();
 
-    //let mut diag_flag = true; //we don't need it
     let mut sub_len = 0_usize;
-    let mut center_flag = false;
+    //let mut center_flag = false;
     let mut left_b: isize = 0;
     let mut right_b: isize = 0;
+    let mut range_start: isize = 0;
+    let mut range_stop: isize = 0;
 
-    let mut padded_shape = a.raw_dim();
-    for (ax, (&ax_len, &[pre_ax, post_ax])) in a.shape().iter().zip(&pad_width).enumerate() {
+    let mut padded_shape = input_array.raw_dim();
+    for (ax, (&ax_len, &[pre_ax, post_ax])) in
+        input_array.shape().iter().zip(&pad_width).enumerate()
+    {
         padded_shape[ax] = ax_len + pre_ax + post_ax;
     }
-    let mut padded = Array::from_elem(padded_shape, const_value);
-    let padded_dim = padded.raw_dim();
+
+    let padded_dim = padded_array.raw_dim();
     //right now this only handles the cross sections
     //since the input is always 0 padded along one axis this
     //works for our use case
 
-    for (ax, (&ax_len, &[pre_ax, post_ax])) in a.shape().iter().zip(&pad_width).enumerate() {
+    for (ax, (&ax_len, &[pre_ax, post_ax])) in
+        input_array.shape().iter().zip(&pad_width).enumerate()
+    {
         //the outer loops controls which axis we are tiling along
         if pre_ax == 0 && post_ax == 0 {
             //diag_flag=false;
@@ -133,119 +140,137 @@ where
 
         a_inv.invert_axis(Axis(ax));
 
-        if pre_ax > 0 {
-            //all "tiles" preceding original along axis
+        range_start = if pre_ax > 0 {
+            0 - (pre_ax / ax_len) as isize
+        } else {
+            0
+        };
+        range_stop = if post_ax > 0 {
+            (post_ax / ax_len) as isize + 1
+        } else {
+            1
+        };
 
-            //get the length of the leftover that won't fit a complete tile
-            sub_len = pre_ax % ax_len;
-            if sub_len != 0 {
-                let mut portion = padded.view_mut();
-                for (axis, &[lo, hi]) in pad_width.iter().enumerate() {
-                    left_b = if ax == axis { 0 } else { lo as isize };
-                    right_b = if ax == axis {
-                        sub_len as isize
-                    } else {
-                        padded_shape[axis] as isize - hi as isize
-                    };
-                    if right_b - left_b != 0 {
-                        portion.slice_axis_inplace(Axis(axis), Slice::from(left_b..right_b));
-                    }
-                }
-                if ((pre_ax / ax_len) + 2) % 2 == 0 {
-                    todo!();
-                } else {
-                    todo!();
-                }
-            }
-            for i in (1..(pre_ax / ax_len) + 1 as usize).rev() {
-                let mut portion = padded.view_mut();
-                for (axis, &[lo, hi]) in pad_width.iter().enumerate() {
-                    //draw a box around the ROI
+        //get the length of the leftover that won't fit a complete tile
+        sub_len = pre_ax % ax_len;
+        if sub_len != 0 {
+            let mut portion = padded_array.view_mut();
+            //need to slice the input as well as the output
+            let mut original_slice = if ((pre_ax / ax_len) + 2) % 2 == 0 {
+                input_array.view()
+            } else {
+                a_inv.view()
+            };
 
-                    right_b = if hi > 0 {
-                        padded_shape[axis] as isize - (hi + i * ax_len) as isize
-                    } else {
-                        padded_shape[axis] as isize - hi as isize
-                    };
-
-                    left_b = if lo > 0 {
-                        lo as isize - (i * ax_len) as isize
-                    } else {
-                        lo as isize
-                    };
-
-                    if right_b - left_b != 0 {
-                        portion.slice_axis_inplace(Axis(axis), Slice::from(left_b..right_b));
-                    }
-                    if i % 2 == 0 {
-                        portion.assign(&a);
-                    } else {
-                        portion.assign(&a_inv);
-                    }
-                }
-            }
+            set_edge::<A, D>(
+                &mut portion,
+                &mut original_slice,
+                sub_len as isize,
+                padded_shape,
+                &pad_width,
+                ax as isize,
+                Some(EdgeRowOp::Top),
+            );
         }
-        if ax == 0 {
-            //only once for the actual original axis
+        for i in range_start..range_stop {
+            let mut portion = padded_array.view_mut();
 
-            let mut orig_portion = padded.view_mut();
             for (axis, &[lo, hi]) in pad_width.iter().enumerate() {
-                orig_portion.slice_axis_inplace(
-                    Axis(axis),
-                    Slice::from(lo as isize..padded_shape[axis] as isize - (hi as isize)),
-                );
-            }
-            orig_portion.assign(&a);
-        }
-        if post_ax > 0 {
-            //now for the tiles to the right of the original
-            for i in 1..(post_ax / ax_len) + 1 as usize {
-                let mut orig_portion = padded.view_mut();
-                for (axis, &[lo, hi]) in pad_width.iter().enumerate() {
-                    right_b = if hi > 0 {
-                        padded_shape[axis] as isize - (hi - i * ax_len) as isize
-                    } else {
-                        padded_shape[axis] as isize - hi as isize
-                    };
+                //draw a box around the ROI
 
-                    left_b = if lo > 0 {
-                        lo as isize + (i * ax_len) as isize
-                    } else {
-                        lo as isize
-                    };
+                right_b = if hi > 0 {
+                    padded_shape[axis] as isize - hi as isize + (i * ax_len as isize)
+                } else {
+                    padded_shape[axis] as isize - hi as isize
+                };
 
-                    orig_portion.slice_axis_inplace(Axis(axis), Slice::from(left_b..right_b));
+                left_b = if lo > 0 {
+                    lo as isize + (i * ax_len as isize)
+                } else {
+                    lo as isize
+                };
+
+                if right_b - left_b != 0 {
+                    portion.slice_axis_inplace(Axis(axis), Slice::from(left_b..right_b));
                 }
                 if i % 2 == 0 {
-                    orig_portion.assign(&a);
+                    portion.assign(&input_array);
                 } else {
-                    orig_portion.assign(&a_inv);
-                }
-            }
-            //check to see if there is any leftover
-            sub_len = post_ax % ax_len;
-            if sub_len != 0 {
-                for (axis, &[lo, hi]) in pad_width.iter().enumerate() {
-                    left_b = if ax == axis {
-                        padded_shape[axis] as isize - sub_len as isize
-                    } else {
-                        lo as isize
-                    };
-                    right_b = if ax == axis {
-                        -1
-                    } else {
-                        padded_shape[axis] as isize - hi as isize
-                    };
-                }
-                if ((post_ax / ax_len) + 2) % 2 == 0 {
-                    todo!();
-                } else {
-                    todo!();
+                    portion.assign(&a_inv);
                 }
             }
         }
+        //check to see if there is any leftover
+        sub_len = post_ax % ax_len;
+        if sub_len != 0 {
+            let mut portion = padded_array.view_mut();
+            let mut original_slice = if ((post_ax / ax_len) + 1) % 2 != 0 {
+                input_array.view()
+            } else {
+                a_inv.view()
+            };
+            set_edge::<A, D>(
+                &mut portion,
+                &mut original_slice,
+                sub_len as isize,
+                padded_shape,
+                &pad_width,
+                ax as isize,
+                Some(EdgeRowOp::Bottom),
+            );
+        }
+
         //flip it back so it works for the next axis
         a_inv.invert_axis(Axis(ax));
     }
-    padded
+}
+
+fn set_edge<A, D>(
+    padded_array: &mut ArrayViewMut<A, D>,
+    input_array: &mut ArrayView<A, D>, //todo: fix this, view should be mutable not the underlying input
+    sub_len: isize,
+    padded_shape: D,
+    pad_width: &Vec<[usize; 2]>,
+    current_axis: isize,
+    row_op: Option<EdgeRowOp>,
+    //col_op: Option<EdgeColOp>,
+) where
+    A: Clone,
+    D: Dimension,
+{
+    for (axis, &[lo, hi]) in pad_width.iter().enumerate() {
+        if current_axis == axis as isize {
+            match row_op {
+                Some(EdgeRowOp::Top) => {
+                    input_array.slice_axis_inplace(Axis(axis), Slice::from(0..sub_len + 1_isize));
+                    padded_array.slice_axis_inplace(Axis(axis), Slice::from(0..sub_len + 1));
+                }
+                Some(EdgeRowOp::Bottom) => {
+                    input_array
+                        .slice_axis_inplace(Axis(axis), Slice::from(0 as isize..sub_len + 1_isize));
+                    padded_array.slice_axis_inplace(
+                        Axis(axis),
+                        Slice::from(
+                            padded_shape[axis] as isize - (sub_len + 1)
+                                ..padded_shape[axis] as isize,
+                        ),
+                    );
+                }
+                None => {
+                    input_array.slice_axis_inplace(Axis(axis), Slice::from(0_isize..));
+                    padded_array.slice_axis_inplace(
+                        Axis(axis),
+                        Slice::from(lo as isize..(padded_shape[axis] - hi) as isize),
+                    );
+                }
+            }
+        } else {
+            input_array.slice_axis_inplace(Axis(axis), Slice::from(0_isize..));
+            padded_array.slice_axis_inplace(
+                Axis(axis),
+                Slice::from(lo as isize..(padded_shape[axis] - hi) as isize),
+            );
+        }
+    }
+    padded_array.assign(&input_array);
 }
